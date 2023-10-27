6d98871d33da3b8c545691ecc22d9f00cfaae906
yoyobara <yotambarak6@gmail.com>
2023-10-27 19:18:11 +0300

    simple color usage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use ncurses:: {
-    initscr, addstr, refresh, getch, endwin
+    initscr, addstr, refresh, getch, endwin, start_color, init_pair, COLOR_WHITE, COLOR_BLACK, COLOR_GREEN, COLOR_RED, attron, attroff, COLOR_PAIR
 };
 
-fn main() {
+const NORMAL_TEXT: i16 = 1;
+const GREEN_TEXT: i16 = 2;
+const RED_TEXT: i16 = 3;
+
+
+/*
+ * kinda initializes the curses screen, colors, etc
+ */
+fn init() {
     initscr();
+    start_color();
+    init_pair(NORMAL_TEXT, COLOR_WHITE, COLOR_BLACK);
+    init_pair(GREEN_TEXT, COLOR_GREEN, COLOR_BLACK);
+    init_pair(RED_TEXT, COLOR_RED, COLOR_BLACK);
+}
+fn main() {
+    init();
 
+    attron(COLOR_PAIR(RED_TEXT));
     addstr("hello world!");
-
+    attroff(COLOR_PAIR(RED_TEXT));
+    
     refresh();
     getch();
 
