a427e30c88e64a50ae835599526a147d0b1d40ae
yoyobara <yotambarak6@gmail.com>
2023-10-29 14:49:11 +0200

    load windows

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use ncurses:: {
-    initscr, addstr, refresh, getch, endwin, start_color, init_pair, COLOR_WHITE, COLOR_BLACK, COLOR_GREEN, COLOR_RED, attron, attroff, COLOR_PAIR
-};
+use ncurses::*;
 
 const NORMAL_TEXT: i16 = 1;
 const GREEN_TEXT: i16 = 2;
@@ -12,19 +10,36 @@ const RED_TEXT: i16 = 3;
  */
 fn init() {
     initscr();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
     start_color();
     init_pair(NORMAL_TEXT, COLOR_WHITE, COLOR_BLACK);
     init_pair(GREEN_TEXT, COLOR_GREEN, COLOR_BLACK);
     init_pair(RED_TEXT, COLOR_RED, COLOR_BLACK);
+
+    refresh();
 }
+
+/*
+ * initializes and draw the input window and the text window.
+ */
+fn initialize_windows() -> (WINDOW, WINDOW) {
+    let input_window = newwin(3, COLS(), 0, 0);
+    let text_window = newwin(LINES() - 3, COLS(), 3, 0);
+
+    box_(input_window, 0, 0);
+    box_(text_window, 0, 0);
+
+    wrefresh(input_window);
+    wrefresh(text_window);
+
+    (input_window, text_window)
+}
+
 fn main() {
     init();
+    let (input_window, text_window) = initialize_windows();
 
-    attron(COLOR_PAIR(RED_TEXT));
-    addstr("hello world!");
-    attroff(COLOR_PAIR(RED_TEXT));
-    
-    refresh();
     getch();
 
     endwin();
