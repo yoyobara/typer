dcb4e3a6bf5f3131a92d56b83bbbc51d78760099
yoyobara <yotambarak6@gmail.com>
2023-11-05 13:18:00 +0200

    explicit boxing

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ fn initialize_windows() -> (WINDOW, WINDOW) {
     let input_window = newwin(3, COLS(), 0, 0);
     let text_window = newwin(LINES() - 3, COLS(), 3, 0);
 
-    box_(input_window, 0, 0);
-    box_(text_window, 0, 0);
+    wborder(input_window, 0, 0, 0, 0, 0, 0, 0, 0);
+    wborder(text_window, 0, 0, 0, 0, 0, 0, 0, 0);
 
     wrefresh(input_window);
     wrefresh(text_window);
@@ -39,7 +39,7 @@ fn initialize_windows() -> (WINDOW, WINDOW) {
     (input_window, text_window)
 }
 
-fn load_text() -> MultiLine {
+fn load_multiline() -> MultiLine {
     let line_length = COLS() - 2; // border duh
     let text = "hello world my name is yotam and I love cookies I work at nistec and I dont know what am I doing with my life.".to_string();
 
@@ -50,18 +50,7 @@ fn load_text() -> MultiLine {
 fn main() {
     init();
     let (input_window, text_window) = initialize_windows();
-
-    getch();
-    let m = load_text();
-
-    for i in 0..m.len() {
-        let sc = m.get(i);
-        let (y, x) = sc.position;
-
-        wattron(text_window, COLOR_PAIR(sc.color));
-        mvwaddch(text_window, y + 1, x + 1, sc.character as u32);
-    }
-    wrefresh(text_window);
+    let multiline = load_multiline();
 
     getch();
 
